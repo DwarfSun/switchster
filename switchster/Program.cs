f379743794e8495418809ca5dd825fcd87ad9a8c
dwarfsun <matt@dwarfsun.co.za>
2018-10-07 10:01:38 +0200

    New Console Project

---
+++ switchster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace switchster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
